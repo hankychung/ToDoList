b378699b55b84e6182872c533e52ad752170b20a
Han <hanky_chung@hotmail.com>
2019-03-24 09:27:36 +0800

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from "react"
-export default class App extends React.Component {
-  render() {
-    return <div>Home</div>
-  }
-}
+import React from 'react'
+
+export default () => (<div>Home</div>)
+// class App extends React.Component {
+//   render() {
+//     return <div>Home</div>
+//   }
+// }
